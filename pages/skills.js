b835997ee8e62f0f3e6a9f7922212b4baf863b8c
lauraziebarth <lauragzie@gmail.com>
2021-02-11 11:33:48 -0300

    add new skills

--- pages/skills.js
+++ pages/skills.js
@@ -1,4 +1,18 @@
 import Link from 'next/link'
+import Html5 from '../public/images/html5.svg'
+import Css3 from '../public/images/css3.svg'
+import JavaScript from '../public/images/javascript.svg'
+import Python from '../public/images/python.svg'
+import MySQL from '../public/images/mysql.svg'
+import Sql from '../public/images/sql.svg'
+import APIRest from '../public/images/apirest.svg'
+import AWS from '../public/images/aws.svg'
+import Kanban from '../public/images/kanban.svg'
+import Kibana from '../public/images/kibana.svg'
+import Grafana from '../public/images/grafana.svg'
+import Sentry from '../public/images/sentry.svg'
+import Idioma from '../public/images/idioma.svg'
+
 
 function Skills() {
     return (
@@ -24,6 +38,63 @@ function Skills() {
 
             <h1>Skills</h1>
 
+            <div className="skills-icons">
+              <div className="languages">
+                <div className="a">
+                  <Html5/><p>HTML 5</p>
+                </div> 
+                <div className="a">
+                  <Css3/><p>CSS 3</p>
+                </div>
+                <div className="a">
+                  <Python/><p>Python</p>
+                </div>
+                <div className="a">
+                  <JavaScript/><p>JavaScript</p>
+                </div>
+              </div>
+
+              <div style={{ borderTop: "2px solid #dedede ", width: "80%"}}></div>
+
+              <div className="tools">
+                <div className="a">
+                  <Sentry/><p>Sentry</p>
+                </div>
+                <div className="a">
+                  <Kibana/><p>Kibana</p>
+                </div>
+                <div className="a">
+                  <Grafana/><p>Grafana</p>
+                </div>
+                <div className="a">
+                  <AWS/><p>AWS</p>
+                </div>        
+              </div>
+
+              <div style={{ borderTop: "2px solid #dedede ", width: "80%"}}></div>
+
+              <div className="others">
+                <div className="a">
+                  <Sql /><p>MySQL</p>
+                </div>
+                <div className="a">
+                  <APIRest/><p>API Rest</p>
+                </div>
+                <div className="a">
+                  <Kanban/><p>Kanban</p>
+                </div>
+              </div>
+
+              <div style={{ borderTop: "2px solid #dedede ", width: "80%"}}></div>
+
+              <div className="soft-skills">
+                <div className="a">
+                  <Idioma/> <p>English (advanced) / Portuguese (native)</p>
+                </div>
+              </div>
+              
+            </div>
+
         </div>
 
   
@@ -36,6 +107,7 @@ function Skills() {
               max-width: 1300px;
               margin: 100px auto;
               justify-content: space-between;
+              padding-left: 70px;
             }
 
             .right-menu {
@@ -64,6 +136,30 @@ function Skills() {
             h1 {
               font-size: 40px;
             }
+
+            .languages, .tools, .others, .soft-skills {
+              display: flex;
+              justify-content: space-between;
+              align-items: center;
+              width: 90%;
+              flex-wrap: wrap;
+              margin-bottom: 30px;
+            }
+
+            .a {
+              display: flex;
+              justify-content: space-between;
+              align-items: center;
+              margin-top: 30px;
+              margin-right: 200px;
+            }
+
+            .a p {
+              margin-left: 17px;
+              font-size: 20px;
+              color: #616161;
+            }
+            
           `}
         </style>
 
