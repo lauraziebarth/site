b2d44809a5a800e171e31577d342cf5676d3091b
lauraziebarth <lauragzie@gmail.com>
2021-02-12 17:10:32 -0300

    create contact infos

--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,7 @@
 import Link from 'next/link'
+import Linkedin from '../public/images/linkedin.svg'
+import Github from '../public/images/github.svg'
+import Twitter from '../public/images/twitter.svg'
 
 function Contact() {
     return (
@@ -22,8 +25,32 @@ function Contact() {
             </Link>
             </div>
 
-            <h1>Contact</h1>
+            <h1>Just say hi.</h1>
+            
+            <h2>I'm always open to talk about new things.</h2>
+            
+            <div className="contact">
+              <div className="email">
+                <p>Mail me at</p>
+                <p>lauragzie@gmail.com</p>
+              </div>
+              
+              <div className="social">
+                <p>Follow me</p>
 
+                <div className="icons">
+                  <a href="https://github.com/lauraziebarth" target="_blank" aria-label="Github">
+                    <Github/>
+                  </a>      
+                  <a href="https://www.linkedin.com/in/laura-gabriela-ziebarth-07ab68115/" target="_blank" aria-label="Linkedin">
+                    <Linkedin/>
+                  </a>
+                  <a href="https://twitter.com/likeamaze" target="_blank" aria-label="Twitter">
+                    <Twitter/>
+                  </a>
+                </div>
+              </div>
+            </div>
         </div>
 
   
@@ -64,6 +91,31 @@ function Contact() {
             h1 {
               font-size: 40px;
             }
+
+            h2 {
+              font-weight: 300;
+              font-size: 27px;
+              margin-top: 15px;
+              color: #858585; 
+            }
+
+            .contact {
+              display: flex;
+              justify-content: space-between;
+              flex-wrap: wrap;
+              max-width: 30%;
+              margin-top: 50px;
+            }
+
+            p:first-child {
+              font-size: 15px;
+              color: #858585;
+            }
+
+            .icons {
+              margin-top: 7px;
+            }
+
           `}
         </style>
       </main>
